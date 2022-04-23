488feee35466897aec1e30dc9f78d615886be1d5
Jenny Spadoni <jennyspadoni@gmail.com>
2022-04-23 12:41:29 -0700

    Added another codewars problem and solution set

---
+++ CW 2022 04 23/app.js
@@ -0,0 +1,23 @@
+/*Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+
+Example:
+h = 0
+m = 1
+s = 1
+
+result = 61000
+Input constraints:
+
+0 <= h <= 23
+0 <= m <= 59
+0 <= s <= 59
+*/
+
+function past(h, m, s){
+    let h1 = h*3600000
+    let m1 = m*60000
+    let s1 = s*1000
+    return (h1 + m1 + s1)
+  }
