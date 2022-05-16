e6e0f1819cb24eda1c878f68066a6ade2788f7d7
Jenny Spadoni <jennyspadoni@gmail.com>
2022-05-16 11:32:47 -0700

    Determine if an input n would lead to a pluralized string or not

---
+++ pluralize.js
@@ -0,0 +1,14 @@
+// We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+
+// You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
+
+// All values will be positive integers or floats, or zero.
+
+//P: integer number or float or zero OR string Infinity
+//R: boolean true/false (true for anything not one)
+//E: plural(0) => true; plural(10) => true; plural(1) => false
+
+function plural(n) {
+    // check if n is != 1; return boolean
+    return n == 1 ? false : true
+  }
