38a631a18b202df7c029bb1368244b1ab0d0ea3b
Jenny Spadoni <jennyspadoni@gmail.com>
2022-05-23 17:14:30 -0700

    Given an array, remove any duplicate numbers and return the modified array (otherwise in the same order)

---
+++ removeArrayDuplicates.js
@@ -0,0 +1,18 @@
+//array of numbers
+//array of other numbers - no duplicates !KEEP THE FIRST NUMBER!
+//E: distinct([2, 1, 1, 2, 1]) => [2, 1]
+//distinct([4, 15, 7, 7, 15, 7, 4, 1, 7, 4, 15]) => [4, 15, 7, 1]
+//distinct([6]) =>
+
+function distinct(a) {
+    //loop through each element and see if it's the last one of it's value
+    //splice out THE LAST ONE if so
+    //Reset the loop back one because everything will shift "left"
+    // return the array
+    for (let i = 0; i < a.length; i++){
+      if(a.lastIndexOf(a[i]) !== i){
+        a.splice(a.lastIndexOf(a[i]), 1);
+        i--
+      }
+    }return a 
+  }
