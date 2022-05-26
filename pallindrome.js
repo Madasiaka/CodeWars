9a7bca878cba38a5545ad14ac00252c7e58ebe2f
Jenny Spadoni <jennyspadoni@gmail.com>
2022-05-25 20:57:04 -0700

    Determine whether a given value is a pallindrome

---
+++ pallindrome.js
@@ -0,0 +1,14 @@
+// Palindrome strings
+// A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. This includes capital letters, punctuation, and word dividers.
+
+// Implement a function that checks if something is a palindrome. If the input is a number, convert it to string first.
+
+// Examples
+// isPalindrome("anna")   ==> true
+// isPalindrome("walter") ==> false
+// isPalindrome(12321)    ==> true
+// isPalindrome(123456)   ==> false
+
+function isPalindrome(line) {
+    return line == line.split("").reverse().join("");
+  }
