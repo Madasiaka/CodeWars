5a99d39b577c4431ccc49a700ef47cd99950797b
Jenny Spadoni <jennyspadoni@gmail.com>
2022-07-29 11:49:22 -0700

    Return a template string integrating two given parameters

---
+++ templateStrings.js
@@ -0,0 +1,10 @@
+/* Task
+Your task is to return the correct string using the Template String Feature.
+Input
+Two Strings, no validation is needed.
+Output
+You must output a string containing the two strings with the word ```' are '``` */
+
+var TempleStrings = function(obj, feature) {
+    return `${obj} are ${feature}`
+  }
