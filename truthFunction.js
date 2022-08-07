8f59d2171fef770a2dc4dad60c5f781d80e53d5a
Jenny Spadoni <jennyspadoni@gmail.com>
2022-08-07 12:39:04 -0700

    Evaluate if an expression is truthy, then call a given function based on the result

---
+++ truthFunction.js
@@ -0,0 +1,7 @@
+/*Create a function called _if which takes 3 arguments: a boolean value bool and 2 functions (which do not take any parameters): func1 and func2
+
+When bool is truth-ish, func1 should be called, otherwise call the func2. */
+
+function _if(bool, func1, func2) {
+    return bool ? func1() : func2()
+  }
