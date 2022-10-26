f47015e5d3d138ec33f7d72e281144631b6a9a49
Jenny Spadoni <jennyspadoni@gmail.com>
2022-10-26 13:58:55 -0700

    Fixed formatting of the question

--- regresionToOne.js
+++ regresionToOne.js
@@ -1,4 +1,6 @@
-/*https://www.codewars.com/kata/541c8630095125aba6000c00/train/javascript#:~:text=Digital%20root%20is,non%2Dnegative%20integer. */
+/* Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer. */
 
 // given a number
 // return a number that is composed of all digits summed until single integer basically 
