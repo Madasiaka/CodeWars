a0cc433a25dc2a311fa5b695e4b6b3dec4970cef
Jenny Spadoni <jennyspadoni@gmail.com>
2022-11-07 11:03:58 -0800

    Optimized results to account for non missing integers in an or statement in less time

--- missingInteger.js
+++ missingInteger.js
@@ -13,11 +13,8 @@ Note: N may be 1 or less (in the latter case, the first array will be []). */
 //return the number missing from the second array (if missing) or 0 if one is not missing
 
 function findDeletedNumber(arr, mixArr) {
-    // compare lengths -if equal return 0; else filter the sorted array for !unsorted.includes(el)
-    if(arr.length == mixArr.length) return 0
-    else{
-      return arr.filter(el => !mixArr.includes(el))[0]
-    }
+    // filter the sorted array for !unsorted.includes(el) - if no missing, it will be falsy so OR 0
+      return arr.filter(el => !mixArr.includes(el))[0] || 0
   }
   
   console.log(findDeletedNumber([1,2,3,4,5], [3,4,1,5]), 2, 'Deletion')
