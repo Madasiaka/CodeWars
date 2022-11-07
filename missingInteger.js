e80f2e8484e577d7d810fbd5807b7b46dc1b67d7
Jenny Spadoni <jennyspadoni@gmail.com>
2022-11-07 11:01:25 -0800

    Given two arrays with integers from 1 to N, determine which integer is missing from the second array (if any)

---
+++ missingInteger.js
@@ -0,0 +1,25 @@
+/*An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+
+Example:
+
+The starting array sequence is [1,2,3,4,5,6,7,8,9]
+The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+Your function should return the int 5.
+If no number was deleted from the starting array, your function should return the int 0.
+
+Note: N may be 1 or less (in the latter case, the first array will be []). */
+
+//two arrays - one sorted and one not - both from 1 to N
+//return the number missing from the second array (if missing) or 0 if one is not missing
+
+function findDeletedNumber(arr, mixArr) {
+    // compare lengths -if equal return 0; else filter the sorted array for !unsorted.includes(el)
+    if(arr.length == mixArr.length) return 0
+    else{
+      return arr.filter(el => !mixArr.includes(el))[0]
+    }
+  }
+  
+  console.log(findDeletedNumber([1,2,3,4,5], [3,4,1,5]), 2, 'Deletion')
+  console.log(findDeletedNumber([1,2,3,4,5,6,7,8,9], [1,9,7,4,6,2,3,8]), 5, 'Deletion')
+  console.log(findDeletedNumber([1,2,3,4,5,6,7,8,9], [5,7,6,9,4,8,1,2,3]), 0, 'No deletion')
